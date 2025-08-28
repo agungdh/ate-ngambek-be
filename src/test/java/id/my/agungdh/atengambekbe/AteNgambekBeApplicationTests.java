9647b68ee152872879f96a738080f3b6ee382966
AgungDH <agungdh@live.com>
2025-08-28 10:20:04 +0700

    init

---
+++ src/test/java/id/my/agungdh/atengambekbe/AteNgambekBeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.atengambekbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteNgambekBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
