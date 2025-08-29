8ca8ae14c5ed7f907d9a9e6102a3ad85b7295145
AgungDH <agungdh@live.com>
2025-08-29 09:20:27 +0700

    feat: add Ngambek & Selesai entities and configure PostgreSQL database
    - Added `Ngambek` and `Selesai` entity classes
    - Updated `application.yml`:
      - Configured PostgreSQL datasource (url, username, password, driver)
      - Enabled Hibernate with `create-drop` ddl-auto option
      - Enabled SQL logging and formatting
      - Set PostgreSQL dialect for JPA

---
+++ src/main/java/id/my/agungdh/atengambekbe/entity/Ngambek.java
@@ -0,0 +1,21 @@
+package id.my.agungdh.atengambekbe.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Ngambek {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private UUID uuid;
+    @Column(nullable = false)
+    private LocalDateTime kapan;
+    @Column(nullable = false)
+    private String kenapa;
+}
---
+++ src/main/java/id/my/agungdh/atengambekbe/entity/Selesai.java
@@ -0,0 +1,24 @@
+package id.my.agungdh.atengambekbe.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Selesai {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private UUID uuid;
+    @OneToOne
+    @JoinColumn(name = "ngambek_id", nullable = false)
+    private Ngambek ngambek;
+    @Column(nullable = false)
+    private LocalDateTime kapan;
+    @Column(nullable = false)
+    private String gimana;
+}
